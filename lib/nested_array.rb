08c39a1733da64f9fe5ac76c8eb177b8f0f62972
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-01-15 01:21:58 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -28,7 +28,14 @@ def assembled_matrix
     "Oranges",
     "Watermelon",
     "Eggplant"
-]
+    ] , [
+    "Strawberries",
+    "Potatoes",
+    "Grapes",
+    "Avocadoes",
+    "Asparagus" 
+    ]
+  ]
 end
 
 def sorted_matrix
